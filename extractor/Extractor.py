c0ad6909cb8ea2ae738d98002773571e341f46cc
Daniil Tulin <Daniil Tulin>
2017-03-23 12:26:36 +0300

    Check format compatible

--- extractor/Extractor.py
+++ extractor/Extractor.py
@@ -1,3 +1,5 @@
+from re import compile, fullmatch
+
 def without_emptys(seq):
     return [s for s in seq if len(s) > 0]
 
@@ -13,10 +15,10 @@ class Node:
         self.expr = None
 
     def is_leaf(self):
-        return self.dep == None
+        return self.dep is None
 
     def cache(self):
-        if self.expr == None:
+        if self.expr is None:
             self.expr = str(self.dep)
         return self.expr
 
@@ -27,6 +29,13 @@ class Node:
             return self.cache()
 
 class LogicGate:
+    def __init__(self):
+        self.input_nodes = None
+        self.output_name = None
+
+        self.input_masks = None
+        self.output_mask = None
+
     def parse(self, text):
         # Split logic gate body
         lines = text.split('\n')
@@ -73,11 +82,13 @@ class LogicGate:
         return wrap(components, result)
 
 class Model:
+    def __init__(self):
+        self.inputs = None
+        self.outputs = None
+        self.logic_gates = None
+
     def parse(self, text):
-        # Filter lines
-        not_comment = lambda l: len(l) > 0 and l[0] != '#'
-        lines = [line for line in text.split('\n')
-                 if not_comment(line)]
+        lines = text.split('\n')
 
         # Take components of blif file
         model_name, input_token, output_token = lines[0:3]
@@ -110,8 +121,41 @@ class Model:
             self.build_dependencies(input_node)
 
 class Extractor:
-    def parse(self, text):
-        self.model = Model().parse(text)
+    def __init__(self):
+        self.model = None
+
+    def parse(self, raw_text):
+        # Filter lines
+        is_valid = lambda l: len(l) > 0 and l[0] != '#'
+        lines_without_comments = [line for line in raw_text.split('\n')
+                                  if is_valid(line)]
+        filtered_text = '\n'.join(lines_without_comments)
+
+        # Header
+        declaration = r'\.model \w+'
+        inputs = r'\.inputs \w+( \w+)*'
+        outputs = r'\.outputs \w+( \w+)*'
+
+        header = r'{}\n'.format('\n'.join([declaration, inputs, outputs]))
+
+        # Body
+        mask = r'([01-]+  [01]\n)+'
+        logic_gate_header = r'\.names \w+([ ]{1,2}\w+)*'
+        logic_gate = r'{}\n{}'.format(logic_gate_header, mask)
+
+        body = r'({})+'.format(logic_gate)
+
+        # End
+        end = r'\.end'
+
+        # Whole blif file
+        model = r'{}{}{}'.format(header, body, end)
+        blif_regex = compile(r'({0}\n)*{0}'.format(model))
+        if fullmatch(blif_regex, filtered_text) is None:
+                raise Exception('Unsupported blif format')
+
+        self.model = Model().parse(filtered_text)
+        return self
 
     def extract(self, variable):
         outputs = self.model.outputs
