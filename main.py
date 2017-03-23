8197303de53a4cc2f3066c18c9c06ad1a285b165
Daniil Tulin <Daniil Tulin>
2017-03-23 12:27:28 +0300

    Show how many files is compatible

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from extractor import Extractor as E
 
+from os import listdir
+from os.path import isfile, join
+files = [join('blifs', f) for f in listdir('blifs') if isfile(join('blifs', f))]
+
 e = E()
+qty = 0
+
+for file in files:
+    with open(file) as f:
+        try:
+            e.parse(f.read())
+            qty += 1
+        except: pass
+print(qty)
 
 with open('b1.blif') as f:
     e.parse(f.read())
-print(e.extract('pf'))
+    print(e.extract('pf'))
