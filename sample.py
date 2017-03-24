076ef28dd726f58822303b8021b8153c5ad8475c
Daniil Tulin <Daniil Tulin>
2017-03-24 15:42:27 +0300

    Script for writing all output variables

--- sample.py
+++ sample.py
@@ -1,20 +1,9 @@
 from extractor import Extractor as E
+from sys import argv
 
-from os import listdir
-from os.path import isfile, join
-files = [join('blifs', f) for f in listdir('blifs') if isfile(join('blifs', f))]
+filename = argv[1]
 
 e = E()
-qty = 0
-
-for file in files:
-    with open(file) as f:
-        try:
-            e.parse(f.read())
-            qty += 1
-        except: pass
-print(qty)
-
-with open('blifs/b1.blif') as f:
+with open(filename) as f:
     e.parse(f.read())
-    print(e.extract('pf'))
+    print(e.extract_all())
