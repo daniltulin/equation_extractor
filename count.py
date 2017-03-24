0b7fd6315fd3b67e7e52085d53b90e27b9ce6450
Daniil Tulin <Daniil Tulin>
2017-03-24 15:42:36 +0300

    Initial commit

---
+++ count.py
@@ -0,0 +1,16 @@
+from extractor import Extractor as E
+from os import listdir
+from os.path import isfile, join
+
+files = [join('blifs', f) for f in listdir('blifs') if isfile(join('blifs', f))]
+
+e = E()
+qty = 0
+
+for file in files:
+    with open(file) as f:
+        try:
+            e.parse(f.read())
+            qty += 1
+        except: pass
+print(qty)
