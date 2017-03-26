1ddd93131bb9a05168faebfcb1dc11a37b393048
Daniil Tulin <Daniil Tulin>
2017-03-27 02:02:37 +0300

    Group and name tokens

--- blifparse/parser.py
+++ blifparse/parser.py
@@ -10,34 +10,36 @@ class Parser():
         identifier = Word(alphas + '_', alphanums + '_')
         identifier_list = OneOrMore(identifier)
 
-        model_name = Suppress('.model') + identifier_list
-        input_list = Suppress('.inputs') + identifier_list
-        output_list = Suppress('.outputs') + identifier_list
+        model_name = Suppress('.model') + identifier('name')
+        input_list = Suppress('.inputs') + Group(identifier_list)('inputs')
+        output_list = Suppress('.outputs') + Group(identifier_list)('outputs')
 
         model_declaration = model_name + input_list + output_list
 
-        variable_list = identifier + Optional(identifier_list)
+        variable_list = (identifier + Optional(identifier_list))
 
-        bit = Word('01-', max=1)
-        mask = Word('01-')
-        output_cover = bit | (mask + bit)
+        bit = Word('01-', max=1)('output_bit')
+        mask = Word('01-')('input_bits')
+        output_cover = Group(bit | (mask + bit))
         output_covers = OneOrMore(output_cover)
 
-        logic_gate = Suppress('.names') + variable_list + output_covers
+        logic_gate = '.names' + Group(variable_list)('variables') +\
+                     Group(output_covers)('masks')
 
         identifier_idc = Suppress(identifier)
-        latch_variable_list = identifier_idc + identifier
+        latch_variable_list = identifier_idc + identifier('output')
         type_control_state = Suppress('re') + identifier_idc + Suppress('2')
-        latch = Suppress('.latch') + latch_variable_list + type_control_state
+        latch = '.latch' + latch_variable_list + type_control_state
 
-        formal_actual = identifier + '=' + identifier
-        formal_list = OneOrMore(formal_actual)
-        library_gate = Suppress('.gate') + identifier + formal_list
+        formal_actual = Group(identifier('formal') + Suppress('=') +
+                              identifier('actual'))
+        formal_list = Group(OneOrMore(formal_actual))
+        library_gate = '.gate' + identifier('name') + formal_list('list')
 
-        command = Group(logic_gate | library_gate | latch)
+        command = Group(logic_gate) | Group(library_gate) | Group(latch)
 
         end = Suppress('.end')
-        model = model_declaration + OneOrMore(command) + end
+        model = model_declaration + Group(OneOrMore(command))('commands') + end
 
         stringEnd = StringEnd()
         blif = OneOrMore(Group(model)) + stringEnd
