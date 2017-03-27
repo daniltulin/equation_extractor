4a1c8412d96f107c6b250107448d77bec849a914
Daniil Tulin <Daniil Tulin>
2017-03-27 15:08:49 +0300

    Move classes to files

--- blifparse/parser.py
+++ blifparse/parser.py
@@ -1,142 +1,8 @@
 from pyparsing import Word, OneOrMore, Suppress, alphanums, alphas
 from pyparsing import StringEnd, Optional, ParserElement, Group
 
-class Bit():
-    def __init__(self, value):
-        self.value = value
-
-    def __repr__(self):
-        try:
-            return str(int(self.value))
-        except:
-            return '-'
-
-    @staticmethod
-    def Factory(tokens):
-        try:
-            return Bit(bool(int(tokens[0])))
-        except:
-            return Bit(None)
-
-class Bits():
-    def __init__(self, bits):
-        self.bits = bits
-
-    def __repr__(self):
-        return ''.join([repr(bit) for bit in self.bits])
-
-    @staticmethod
-    def Factory(tokens):
-        return Bits([Bit.Factory([t]) for t in tokens[0]])
-
-class Variable():
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return "Var: '" + self.name + "'"
-
-    @staticmethod
-    def Factory(tokens):
-        return Variable(tokens[0])
-
-class InputVariable(Variable):
-    def __repr__(self):
-        return 'Input' + Variable.__repr__(self)
-
-class ConstantVariable(Variable):
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-    def __repr__(self):
-        return "Const" + Variable.__repr__(self) + ", " + repr(self.value)
-
-class LogicGate():
-    def __init__(self, inputs, output, masks, output_mask):
-        self.inputs = inputs
-        self.output = output
-        self.masks = masks
-        self.output_mask = output_mask
-
-    def __repr__(self):
-        return 'Logic gate: inputs: ' + repr(self.inputs) + ', output: ' +\
-                repr(self.output) + ', masks: ' + repr(self.masks) +\
-                ', output_mask: ' + repr(self.output_mask)
-
-    @staticmethod
-    def Factory(result):
-        variables = result.variables
-        inputs = variables[:-1]
-        output = variables[-1]
-
-        covers = result.covers
-
-        if len(inputs) == 0:
-            bit = covers[0].output_bit
-            if bit.value is None:
-                return InputVariable(output.name)
-            else:
-                return ConstantVariable(output.name, bit)
-
-        masks = [cover.mask for cover in covers]
-        output_mask = Bits([cover.output_bit for cover in covers])
-        return LogicGate(inputs, output, masks, output_mask)
-
-class LibraryGate():
-    def __init__(self, model_name, assignments):
-        self.model_name = model_name
-        self.assignments = assignments
-
-    def __repr__(self):
-        return 'Library gate: ' + self.model_name + ', ' +\
-                ', '.join([a.name + '=' + b.name
-                           for a, b in self.assignments.items()])
-
-    @staticmethod
-    def Factory(result):
-        model_name = result.model_name
-        assignments = {assignment.actual: assignment.formal
-                       for assignment in result.assignments}
-        return LibraryGate(model_name, assignments)
-
-class Latch():
-    @staticmethod
-    def Factory(result):
-        return InputVariable(result.output)
-
-class Model():
-    def __init__(self, name, inputs, constants, outputs, commands):
-        self.name = name
-        self.inputs = inputs
-        self.constants = constants
-        self.outputs = outputs
-        self.commands = commands
-
-    def __repr__(self):
-        return 'Model: ' + self.name + ' ' +\
-                ', '.join(map(repr, [self.inputs, self.constants,
-                                    self.outputs, self.commands]))
-
-    @staticmethod
-    def Factory(result):
-        vectorize = lambda seq: [x for x in seq]
-        name = result.name
-        inputs = vectorize(result.inputs)
-        outputs = vectorize(result.outputs)
-
-        components = result.components
-        filter_components = lambda f: [c for c in components if f(c)]
-
-        commands = filter_components(lambda c: isinstance(c, LibraryGate) or\
-                                               isinstance(c, LogicGate))
-        constants = filter_components(lambda c: isinstance(c, ConstantVariable))
-        input_variables = \
-                filter_components(lambda c: isinstance(c, InputVariable))
-
-        return Model(name, inputs + input_variables,
-                     constants, outputs, commands)
-
+from .components import LogicGate, LibraryGate, Variable, InputVariable,\
+                        ConstantVariable, Bit, Bits, Latch, Model
 
 class Parser():
     def parse(self, f):
@@ -144,15 +10,19 @@ class Parser():
         filtered_lines = [l for l in f if is_valuable_line(l)]
         text = ' '.join(filtered_lines).replace('\\', ' ')
 
-        identifier = Word(alphas + '_', alphanums + '_')
         plain_name = Word(alphas + '_', alphanums + '_')
+        identifier = plain_name.copy()
 
         model_name = Suppress('.model') + plain_name('name')
 
         identifier.setParseAction(Variable.Factory)
         identifier_list = OneOrMore(identifier)
 
-        input_list = Suppress('.inputs') + Group(identifier_list)('inputs')
+        input_variable = plain_name.copy()
+        input_variable.setParseAction(InputVariable.Factory)
+        input_variable_list = OneOrMore(input_variable)
+
+        input_list = Suppress('.inputs') + Group(input_variable_list)('inputs')
         output_list = Suppress('.outputs') + Group(identifier_list)('outputs')
 
         model_declaration = model_name + input_list + output_list
@@ -165,7 +35,7 @@ class Parser():
         bit.setParseAction(Bit.Factory)
         mask.setParseAction(Bits.Factory)
 
-        output_cover = Group(bit | (mask + bit))
+        output_cover = Group((mask + bit) | bit)
         output_covers = OneOrMore(output_cover)
 
         logic_gate = Suppress('.names') + Group(variable_list)('variables') +\
@@ -178,8 +48,8 @@ class Parser():
         latch = Suppress('.latch') + latch_variable_list + type_control_state
         latch.setParseAction(Latch.Factory)
 
-        formal_actual = Group(identifier('formal') + Suppress('=') +
-                              identifier('actual'))
+        formal_actual = Group(plain_name('formal') + Suppress('=') +
+                              plain_name('actual'))
         assignments = Group(OneOrMore(formal_actual))
         library_gate = Suppress('.gate') + plain_name('model_name') +\
                        assignments('assignments')
@@ -192,7 +62,12 @@ class Parser():
         model.setParseAction(Model.Factory)
 
         stringEnd = StringEnd()
-        blif = OneOrMore(Group(model)) + stringEnd
+        blif = OneOrMore(model) + stringEnd
 
         result = blif.parseString(text)
+
+        models = {m.name: m for m in result}
+        for model in models.values():
+            model.link_with(models)
+
         return result
